f245795c46b213c5dad8f1ad5cb6b8dad3a15fcb
asciiu <mcat349@ymail.com>
2010-10-22 10:45:17 -0600

    fixed init bug

--- init.rb
+++ init.rb
@@ -2,4 +2,4 @@
 # can use that code via a native call 
 ActionController::Base.send :include, JqueryInPlaceEditing
 # do the same for the helper so it is available to the application
-ActionController::Base.helper JqueryInEditingPlaceMacrosHelper
+ActionController::Base.helper JqueryInPlaceEditingMacrosHelper
